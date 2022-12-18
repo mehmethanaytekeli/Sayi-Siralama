739ae35b941012577ec88786a1e03986521ffec1
Mehmethan AYTEKELİ <mehmethanaytekeli@gmail.com>
2022-12-18 15:24:53 +0300

    Create Sayı_Sıralama.py

---
+++ Sayı_Sıralama.py
@@ -0,0 +1,11 @@
+a = int(input("1.sayıyı girin :"))
+
+b = int(input("2.sayıyı girin :"))
+
+c = int(input("3.sayıyı girin :"))
+
+liste = [a,b,c]
+
+print("En Küçük Sayı :{}".format(min(liste)))
+print("En Büyük Sayı :{}".format(max(liste)))
+
